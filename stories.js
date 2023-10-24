0cc2a9b4e0b9815d1e98a0f80008d02cb839b8b4
paige <paigechavarria@LAPTOP-QA85VTS5>
2023-10-24 15:10:39 -0600

    attempt on stories form

---
+++ stories.js
@@ -0,0 +1,87 @@
+"use strict";
+
+// This is the global list of the stories, an instance of StoryList
+let storyList;
+
+/** Get and show stories when site first loads. */
+
+async function getAndShowStoriesOnStart() {
+  storyList = await StoryList.getStories();
+  $storiesLoadingMsg.remove();
+
+  putStoriesOnPage();
+}
+
+/**
+ * A render method to render HTML for an individual Story instance
+ * - story: an instance of Story
+ *
+ * Returns the markup for the story.
+ */
+
+function generateStoryMarkup(story) {
+  // console.debug("generateStoryMarkup", story);
+
+  let star = favoriteStar(story, currentUser)
+  const hostName = story.getHostName();
+  return $(`
+      <li id="${story.storyId}">
+      ${star}
+        <a href="${story.url}" target="a_blank" class="story-link">
+          ${story.title}
+        </a>
+        <small class="story-hostname">(${hostName})</small>
+        <small class="story-author">by ${story.author}</small>
+        <small class="story-user">posted by ${story.username}</small>
+      </li>
+    `);
+}
+
+function favoriteStar(story, user) {
+  const isFavorite = user.isFavorite(story);
+  let starType = '';
+  if(isFavorite === true){
+    starType = 'fas';
+  }
+  else {
+    starType = 'far';
+  }
+  return `<span class="star">
+            <i class="${starType} fa-star"></i>
+          </span>`;
+}
+
+/** Gets list of stories from server, generates their HTML, and puts on page. */
+
+function putStoriesOnPage() {
+  console.debug("putStoriesOnPage");
+
+  $allStoriesList.empty();
+
+  // loop through all of our stories and generate HTML for them
+  for (let story of storyList.stories) {
+    const $story = generateStoryMarkup(story);
+    $allStoriesList.append($story);
+  }
+
+  $allStoriesList.show();
+}
+
+async function submitStory(e) {
+  e.preventDefault();
+  let author = $('#author').val();
+  let title = $('#title').val();
+  let url = $('#url').val();
+  let username = currentUser.username;
+
+  const newStory = await storyList.addStory(currentUser, {author, title, url, username})
+  
+  const addStory = generateStoryMarkup(newStory)
+  $('.stories-list').prepend(addStory);
+  
+}
+
+const submitForm = document.querySelector('#submitForm');
+submitForm.addEventListener('submit', function(e){
+  console.log('its also working');
+})
